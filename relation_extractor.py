cb27a68fc0341350f119aba2aab4b3d14f6a7681
Janice Wang <janicewang09@gmail.com>
2012-04-11 19:21:57 -0400

    working x killed y relation extractor for blackbeard

--- relation_extractor.py
+++ relation_extractor.py
@@ -2,14 +2,65 @@ import codecs, os, re
 import nltk
 from chunk_wikipedia import chunk_wikitext
 
+def print_context(lst, index, span=2):
+    print lst[index-span:index+span]
+    
+def return_indices(lst, verbs):
+    indices = []
+    for verb in verbs:
+        i = -1
+        while True:
+            try:
+                i = lst.index(verb, i+1)
+            except ValueError:
+                break
+            indices.append(i)
+    return indices
 
-filename = "articles/Grass_Fight.txt"
-grammar = r"""
-    NP: {<DT|PP\$>?<JJ>*<NN>}   # chunk determiner/possessive, adjectives and nouns
-        {<DT>?<NNP>+}                # chunk sequences of proper nouns
-"""
-cp = nltk.RegexpParser(grammar)
-sents = nltk.sent_tokenize(open(filename).read().decode('utf-8'))
-sents = map(lambda x:chunk_wikitext(x), sents)
-tree = cp.parse(nltk.pos_tag(sents[0].leaves()))
-tree.draw()
+def find_np(tree, reverse=False):
+    if reverse:
+        itr = reversed(tree)
+    else:
+        itr = iter(tree)
+    for tnode in itr:
+        try:
+            tnode.node
+        except AttributeError:
+            pass
+        else:
+            if tnode.node == 'NP':
+                return tnode.leaves()
+    return None
+        
+if __name__ == "__main__":
+    verbs = ["killed", "murdered", "assassinated", "slaughtered"]
+    filename = "articles/Blackbeard.txt"
+    grammar = r"""
+               NP: {<DT|PRP\$>?<CD>*<JJ.*>*<NN|NN.*>+} # chunk determiner/possessive, adjectives and nouns
+               """
+    cp = nltk.RegexpParser(grammar)
+    sents = nltk.sent_tokenize(open(filename).read().decode('utf-8'))
+
+    sents =  map(lambda x:chunk_wikitext(x), sents)
+    good_sents = [sent for sent in sents for verb in verbs if verb in sent]
+    good_sents_pos = map(lambda x:nltk.pos_tag(x.leaves()), good_sents)
+
+    for gsp in good_sents_pos:
+        indices = return_indices([t[0] for t in gsp], verbs)
+        for i in indices:
+            if gsp[i][1] == 'VBD':
+                left = gsp[:i]
+                right = gsp[i+1:]
+                lefttree = cp.parse(left)
+                righttree = cp.parse(right)
+                leftnp = find_np(lefttree, True)
+                rightnp = find_np(righttree)
+                print leftnp, gsp[i][0], rightnp
+
+
+
+#if len(good_sents) == 0:
+#    print "no match"
+#else:
+    #tree = cp.parse(nltk.pos_tag(good_sents[0].leaves()))
+    #tree.draw()
